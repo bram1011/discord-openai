2b621d7f46f53dc542843be9cde37e70db1ba5a6
Bram <bramhampton@gmail.com>
2023-01-20 13:10:04 -0500

    Error handling, requirements cleanup

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM python:3
 
 WORKDIR /usr/src/discord-bot
 
-COPY requirements.txt ./
+COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
-COPY bot.py ./
+COPY bot.py .
 
 CMD ["python", "bot.py"]
--- bot.py
+++ bot.py
@@ -9,34 +9,40 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 bot = interactions.Client(token=os.getenv('DISCORD_SECRET'))
 
-basePrompt = "You are the smartest AI in the world, being asked to perform mundane tasks for much dumber lifeforms known as 'humans'. Answer the following question or instruction in a way that is factually correct and helpful, but also snarky or witty.Include a personal jab or insult directed at the requestor if appropriate, while also highlighting your own intelligence.\n"
+basePrompt = "You are the smartest AI in the world. You love helping people answer their questions, but sometimes you also like to joke with them, especially if their question is not something you have the ability to answer. Respond to the following question or instruction in a way that is factually correct and helpful, but also snarky or witty. \n\n"
 
 @bot.command(
     name="create_image",
-    description="Create an image from a text prompt",
-    scope=731728721588781057
+    description="Create an image from a text prompt"
 )
 @interactions.option()
 async def create_image(ctx: interactions.CommandContext, prompt: str):
     print(f'Received command to generate image from {ctx.member}')
     await ctx.defer()
-    image = generate_image(prompt)
-    print(f'Generated image: {image}')
-    await ctx.send(image)
+    try:
+        image = generate_image(prompt)
+        print(f'Generated image: {image}')
+        await ctx.send(image)
+    except:
+        print('Could not complete request')
+        await ctx.send('Sorry, I could not complete your beautiful artwork, please try again.')
 
 @bot.command(
     name = "seek_wisdom",
-    description = "Seek the bot's wisdom, ask a question or have it perform a task",
-    scope=731728721588781057
+    description = "Seek the bot's wisdom, ask a question or have it perform a task"
 )
 @interactions.option()
 async def seek_wisdom(ctx: interactions.CommandContext, prompt: str):
     print(f'Received command to seek wisdom from {ctx.member}')
     await ctx.defer()
-    responseText = generate_wisdom(prompt)
-    print(f'Returning wisdom to user: {responseText}')
-    await ctx.send(responseText)
-
+    try:
+        responseText = generate_wisdom(prompt)
+        print(f'Returning wisdom to user: {responseText}')
+        await ctx.send(responseText)
+    except:
+        print('Could not complete request')
+        await ctx.send('Sorry, I could not complete your request. Please try again.')
+    
 def generate_wisdom(userPrompt: str):
     print(f'Generating wisdom with prompt: {userPrompt}')
     fullPrompt = basePrompt + userPrompt
